b96dc0d6223cb895d89f7d343c9c6aa1f131b44f
sanjayraoms <sanjayraoms@gmail.com>
2024-07-28 10:13:34 +0530

    Examples of Delegates, Action, Func, anonymous and lambda methods

---
+++ Delegates/CSharpFeatures/Delegates/DelegateTesting.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class DelegateTesting
+    {
+        public delegate void TestDelegate();
+        public delegate bool DelegatewithParamReturn(int val);
+
+        public TestDelegate testDelegate;
+        public DelegatewithParamReturn delegatewithParamReturn;
+
+        //Built-in delegates
+        public Action TestAction;
+        public Action<int,float> TestFunc;
+        
+        //In Func delegate last parameter is a return type
+        public Func<int,bool> TestFuncReturn;
+        public void MyFunc1()
+        {
+            Console.WriteLine("MyFunc1");
+        }
+
+        public void MyFuncNew()
+        {
+            Console.WriteLine("MyFuncNew");
+        }
+
+        public bool TestDel(int num)
+        {
+            return num < 5;
+        }
+    }
+}
---
+++ Delegates/CSharpFeatures/Delegates/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Delegates;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine("Hello, World!");
+
+var d = new DelegateTesting();
+
+d.testDelegate = d.MyFunc1;
+d.testDelegate += d.MyFuncNew;
+
+Console.WriteLine("Multicast delegate results after adding two functions");
+d.testDelegate();
+
+d.testDelegate -= d.MyFuncNew;
+
+Console.WriteLine("Multicast delegate results after removing one function");
+d.testDelegate();
+
+Console.WriteLine("Delegate with parameter and return type");
+d.delegatewithParamReturn = d.TestDel;
+d.delegatewithParamReturn(3);
+
+
+d.testDelegate = delegate () { Console.WriteLine("Anonymous Function"); };
+d.testDelegate += () => { Console.WriteLine("Lambda Function"); };
+d.testDelegate();
+
+d.delegatewithParamReturn = (int j) => { return j < 5; };
+Console.WriteLine(d.delegatewithParamReturn(3));
+
+d.TestFunc = (int i, float j) => Console.WriteLine("Build-in delegates");
+
+d.TestFuncReturn = (int j) => { return j < 5; };
+Console.WriteLine(d.TestFuncReturn(8)); 
+
+
+
+Console.Read();
+
+
